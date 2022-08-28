37c8c9bdbc40c965bc54141fa9fa17829cd32b6a
Timothy <tkkibet13@gmail.com>
2022-08-28 12:28:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-function fetchBooks() {
+const fetchBooks =() =>{
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp)=>resp.json())
+  .then((bookTitles)=>renderBooks(bookTitles))
+  // return fetch("http://localhost:3000/users")
   
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
   books.forEach(book => {
+    const main = document.querySelector('main');
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML += book.name;
     main.appendChild(h2);
+   
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks());
